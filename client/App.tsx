fca5c6203d111eadf14801baa1ced52837a3e42d
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-09-09 15:02:32 +0200

    added markers

--- client/App.tsx
+++ client/App.tsx
@@ -7,7 +7,7 @@ import MapLeaflet from "./components/MapLeaflet";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Tently!!!</Text>
+      <Text style={styles.header}>Welcome to Tently!</Text>
       <MapLeaflet></MapLeaflet>
       <StatusBar style="auto" />
     </View>
@@ -17,8 +17,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#52aca2",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#062542",
   },
 });
--- client/components/MapLeaflet/index.web.tsx
+++ client/components/MapLeaflet/index.web.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 import "./MapLeaflet.css";
 import { useMapLeaflet } from "./hooks";
+import markers from "./markers.json";
+import { Icon } from "leaflet";
 
 export default () => {
   const { mapCenterPosition, zoom } = useMapLeaflet();
@@ -12,11 +14,16 @@ export default () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
       />
-      <Marker position={mapCenterPosition}>
-        <Popup>
-          Popup <br /> description.
-        </Popup>
-      </Marker>
+      {markers.map((marker) => (
+        <Marker
+          position={marker.position}
+          icon={new Icon({ iconUrl: marker.icon, iconSize: marker.size })}
+        >
+          <Popup>
+            Popup <br /> description.
+          </Popup>
+        </Marker>
+      ))}
     </LeafletMap>
   );
 };
