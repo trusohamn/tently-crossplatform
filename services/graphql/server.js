1a7492db78a2d0b65e2bc5f1ef4d4814f8325639
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-10-05 13:46:02 +0200

    mutation

--- services/graphql/server.js
+++ services/graphql/server.js
@@ -2,10 +2,25 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+const db = {};
 const schema = buildSchema(`
+  input LocationInput {
+    description: String
+  }
+
+  type Location {
+    id: ID!
+    description: String
+  }
+
   type Query {
     multiply(x: Int!, y: Int!): Int
     add(x: Int!, y: Int!): Int
+    getLocation(id: Int!): Location
+}
+
+  type Mutation {
+    createLocation(id:Int, location: LocationInput!): Location
   }
 `);
 
@@ -15,6 +30,12 @@ const root = {
   },
   add: ({ x, y }) => {
     return x + y;
+  },
+  getLocation: ({ id }) => db[id],
+  createLocation: ({ location }) => {
+    const id = require("crypto").randomBytes(10).toString("hex");
+    db[id] = location;
+    return { id, ...location };
   },
 };
 
