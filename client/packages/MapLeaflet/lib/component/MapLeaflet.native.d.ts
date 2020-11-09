96377dd2e3ba1afdfbf1853ce8e9038880a73e77
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-11-09 10:31:09 +0100

    cleaned up dependencies

--- client/packages/MapLeaflet/lib/component/MapLeaflet.native.d.ts
+++ client/packages/MapLeaflet/lib/component/MapLeaflet.native.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="react" />
 import { MapLeafletProps } from '../types';
 declare const MapLeaflet: ({ markers, zoom: zoomSetting, position: positionSetting, selectedPosition, setSelectedPosition, markerIcon, }: MapLeafletProps) => JSX.Element;
 export default MapLeaflet;
