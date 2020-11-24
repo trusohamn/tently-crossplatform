c5501e5d99e47034c52b399ffd2e76e7e211259b
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-11-24 11:59:08 +0100

    copied classes from tutorial

--- services/graphql/server.ts
+++ services/graphql/server.ts
@@ -5,6 +5,89 @@ import { buildSchema } from 'graphql'
 import cors from 'cors'
 import db from './db'
 import { LocationInput } from './types'
+import {
+  Arg,
+  Args,
+  ArgsType,
+  Field,
+  ID,
+  InputType,
+  Int,
+  ObjectType,
+  Query,
+  Resolver,
+} from 'type-graphql'
+import {
+  MaxLength,
+  Length,
+  ArrayMaxSize,
+  Min,
+  Max,
+} from 'class-validator'
+
+@ObjectType()
+class Recipe {
+  @Field((type) => ID)
+  id: string
+
+  @Field()
+  title: string
+
+  @Field({ nullable: true })
+  description?: string
+
+  @Field()
+  creationDate: Date
+
+  @Field((type) => [String])
+  ingredients: string[]
+}
+
+@InputType()
+class NewRecipeInput {
+  @Field()
+  @MaxLength(30)
+  title: string
+
+  @Field({ nullable: true })
+  @Length(30, 255)
+  description?: string
+
+  @Field((type) => [String])
+  @ArrayMaxSize(30)
+  ingredients: string[]
+}
+
+@ArgsType()
+class RecipesArgs {
+  @Field((type) => Int)
+  @Min(0)
+  skip: number = 0
+
+  @Field((type) => Int)
+  @Min(1)
+  @Max(50)
+  take: number = 25
+}
+
+@Resolver(Recipe)
+class RecipeResolver {
+  constructor(private recipeService: any) {}
+
+  @Query((returns) => Recipe)
+  async recipe(@Arg('id') id: string) {
+    const recipe = await this.recipeService.findById(id)
+    if (recipe === undefined) {
+      throw new Error(id)
+    }
+    return recipe
+  }
+
+  @Query((returns) => [Recipe])
+  recipes(@Args() { skip, take }: RecipesArgs) {
+    return this.recipeService.findAll({ skip, take })
+  }
+}
 
 const schema = buildSchema(`
 type Position {
