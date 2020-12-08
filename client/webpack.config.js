338a271b9a018965072c850d09dcf533f1ee07b9
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-12-08 20:40:13 +0100

    webpack config not needed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,11 +4,11 @@ module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(
     {
       ...env,
-      babel: {
+      /*       babel: {
         dangerouslyAddModulePathsToTranspile: [
           'mapleaflet-react-web-native',
         ],
-      },
+      }, */
     },
     argv,
   )
