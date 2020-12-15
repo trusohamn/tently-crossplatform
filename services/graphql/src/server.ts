63f1a3c4d9936984e954e6e4b77306b552b690e0
Marta Trusohamn <marta.trusohamn@gmail.com>
2020-12-15 20:58:15 +0100

    works in docker

--- services/graphql/src/server.ts
+++ services/graphql/src/server.ts
@@ -10,7 +10,6 @@ const bootstrap = async () => {
   await createConnection({
     username: process.env.DB_USERNAME,
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: 5432,
     type: 'postgres',
