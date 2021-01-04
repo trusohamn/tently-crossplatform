9c7e786533ff0f853b08e088c1e84619c47c2378
Marta Trusohamn <marta.trusohamn@gmail.com>
2021-01-04 21:32:22 +0100

    heroku setup tsc

--- client/constants.ts
+++ client/constants.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native'
 
-export const service =
-  Platform.OS === 'web'
+export const service = 'https://tently.herokuapp.com/graphql'
+/*  Platform.OS === 'web'
     ? 'http://localhost:4000/graphql'
-    : 'http://10.0.2.2:4000/graphql'
+    : 'http://10.0.2.2:4000/graphql' */
